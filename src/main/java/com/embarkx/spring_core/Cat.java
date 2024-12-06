3344cf26a6bd73d841a6f23a669aaf223d2d12e3
sudipta <sudipta_seal08@yahoo.com>
2024-12-06 17:49:49 +0530

    primary component

--- src/main/java/com/embarkx/spring_core/Cat.java
+++ src/main/java/com/embarkx/spring_core/Cat.java
@@ -1,5 +1,10 @@
 package com.embarkx.spring_core;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
 public class Cat implements Animal{
     @Override
     public void Play() {
