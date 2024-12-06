3e092722b81aaa8c774362fcf88deaef865d2738
sudipta <sudipta_seal08@yahoo.com>
2024-12-06 17:42:46 +0530

    ComponenetScan

--- src/main/java/com/embarkx/spring_core/SpringCoreApplication.java
+++ src/main/java/com/embarkx/spring_core/SpringCoreApplication.java
@@ -3,8 +3,10 @@ package com.embarkx.spring_core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.embarkx.spring_core", "test"})
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
