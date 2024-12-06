e34ee1f7237a1583ad12c5290be995075188e0cf
sudipta <sudipta_seal08@yahoo.com>
2024-12-06 17:53:56 +0530

    ComponentScan

--- src/main/java/com/embarkx/spring_core/SpringCoreApplication.java
+++ src/main/java/com/embarkx/spring_core/SpringCoreApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import test.Test;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.embarkx.spring_core", "test"})
+@ComponentScan(basePackages = {"com.embarkx.spring_core","test"})
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
@@ -14,6 +15,7 @@ public class SpringCoreApplication {
 		ApplicationContext applicationContext = SpringApplication.run(SpringCoreApplication.class, args);
 		Person person = applicationContext.getBean(Person.class);
 		person.playWith();
+		Test testBean = applicationContext.getBean(Test.class);
 	}
 
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Test {
+
+    public void testing() {
+        System.out.println("Doing testing");
+    }
 }
